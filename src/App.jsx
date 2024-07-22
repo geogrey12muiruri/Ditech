d2e46e18140ec72236a208838e129c679af17f71
geogrey12muiruri <greggambugua@gmail.com>
2024-07-22 18:50:35 +0300

    added benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-// import Benefits from "./components/Benefits";
-// import Collaboration from "./components/Collaboration";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 // import Footer from "./components/Footer";
 import Header from "./components/header/Header";
-// import Hero from "./components/Hero";
+import Hero from "./components/Hero";
 // import Pricing from "./components/Pricing";
 // import Roadmap from "./components/Roadmap";
 // import Services from "./components/Services";
@@ -13,13 +13,13 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        {/* <Hero />
-        <Benefits />
+      <Hero />
+         <Benefits />
         <Collaboration />
-        <Services />
+        {/* <Services />
         <Pricing />
         <Roadmap />
-        <Footer /> */}
+        <Footer />    */}
       </div>
 
       <ButtonGradient />
